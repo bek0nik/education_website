cba83c69ddf42b8b03ed3da5d4d11f62fb7612a0
bek <obozrevatel90@gmail.com>
2012-03-14 11:31:06 +0600

    css and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 EducationWebsite::Application.routes.draw do
+  get "pages/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +51,7 @@ EducationWebsite::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root :to => 'pages#index'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.

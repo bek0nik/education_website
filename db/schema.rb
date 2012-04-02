abfd18d6f12b62bd5045a058908339087c4f3fc5
bek <obozrevatel90@gmail.com>
2012-04-02 19:10:11 +0600

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120326035158) do
+ActiveRecord::Schema.define(:version => 20120402111835) do
 
   create_table "questions", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(:version => 20120326035158) do
     t.string   "correct"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "variant_3"
+    t.string   "variant_4"
   end
 
   create_table "test_names", :force => true do |t|

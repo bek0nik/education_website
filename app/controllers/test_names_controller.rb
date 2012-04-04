cd3c92c420b1687350e0643b5c90825b93fe2f5e
bek <obozrevatel90@gmail.com>
2012-04-04 11:56:48 +0600

    predzashita 1

--- app/controllers/test_names_controller.rb
+++ app/controllers/test_names_controller.rb
@@ -6,9 +6,26 @@ class TestNamesController < ApplicationController
     @tests = TestName.all
   end
   
+  def show
+    @test = TestName.find(params[:id])    
+  end
+  
   def start            
+    session[:question_id] ||= {} 
     @test = TestName.find(params[:id])
-    @question = @test.questions.all
+    @question = @test.questions.all.sample(30)
+    session[:question_id] = @question
+    session[:test_step].deep_merge!(params[:f]) if params[:f]
+    @test.current_step = session[:test_step]
+    if params[:back_button]
+      @test.previous_step
+#    elsif @test.last_step?
+#      @test.result
+    else
+      @test.next_step      
+    end
+    session[:test_step] = @test.current_step
+    
       
   end
   
@@ -16,6 +33,4 @@ class TestNamesController < ApplicationController
       
   end
   
-  
-
 end
--- app/models/test_name.rb
+++ app/models/test_name.rb
@@ -1,3 +1,27 @@
 class TestName < ActiveRecord::Base
   has_many :questions
+  attr_writer :current_step
+  def current_step
+    @current_step ||= steps.first    
+  end
+  
+  def steps
+    %w[one second  third fourth thirtieth]    
+  end
+  
+  def next_step
+    self.current_step = steps[steps.index(current_step)+1]    
+  end
+  
+  def previous_step
+    self.current_step = steps[steps.index(current_step)-1]    
+  end
+  
+  def first_step?
+    current_step == steps.first    
+  end
+  
+  def last_step?
+    current_step == steps.last    
+  end
 end

6efc7c5292eb8685d40963afeb17f36087ca2830
bek <obozrevatel90@gmail.com>
2012-04-09 16:45:21 +0600

    frindly url for session

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ EducationWebsite::Application.routes.draw do
 
   resources :users
   resources :sessions
+  
+  get "signup", :to => 'user#new', :as => 'signup'
+  get "login", :to => 'sessions#new', :as => 'login'
+  get "logout", :to => 'sessions#destroy', :as => 'logout'
 
   resources :questions
 

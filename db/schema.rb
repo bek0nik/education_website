375104f37175bdb497c5b28e3eed429dd3a0652d
bek <obozrevatel90@gmail.com>
2012-05-06 16:09:29 +0600

    add in table users column role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120505180403) do
+ActiveRecord::Schema.define(:version => 20120506100322) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -157,8 +157,9 @@ ActiveRecord::Schema.define(:version => 20120505180403) do
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+    t.boolean  "role",            :default => false
   end
 
 end

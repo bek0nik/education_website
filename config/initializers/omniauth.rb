2017eb9df95a4f545f2fa0ae8e3a24054651a250
bek <obozrevatel90@gmail.com>
2012-05-18 13:24:45 +0600

    facebook secret numbers

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['361123313948615'], ENV['4df96223d2c848558d52d9d72e010de1']
   provider :vkontakte, ENV['API_KEY'], ENV['API_SECRET']
 end

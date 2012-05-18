29932bb8de9d230d9c82eaa995fceeb517bdd887
bek <obozrevatel90@gmail.com>
2012-05-18 13:52:45 +0600

    fix

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,3 +1,4 @@
+#encoding:UTF-8
 class AuthenticationsController < ApplicationController
   
   def index
--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,3 +1,4 @@
+#encoding:UTF-8
 class Authentication < ActiveRecord::Base
   belongs_to :user
 end

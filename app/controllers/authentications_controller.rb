14c389a4abf1c014b511e5b3955f8d257c5467e9
bek <obozrevatel90@gmail.com>
2012-05-24 18:14:53 +0600

    fix omniauth 1

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -6,9 +6,10 @@ class AuthenticationsController < ApplicationController
   end
   
   def create
-    auth = request.env["omniauth.auth"] 
-    current_user.authentications.create(:provider => auth['provider'], :uid => auth['uid'])
-    flash[:notice] = "Authentication successful."
-    redirect_to authentications_url    
+    render :text => request.env["omniauth.auth"].to_yaml
+#    auth = request.env["omniauth.auth"] 
+#    current_user.authentications.create(:provider => auth['provider'], :uid => auth['uid'])
+#    flash[:notice] = "Authentication successful."
+#    redirect_to authentications_url    
   end
 end
